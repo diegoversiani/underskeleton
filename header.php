ce51c8fa0a6aa3b185cfc4334f20b58d29a44536
Diego Versiani <diegoversiani@gmail.com>
2016-06-01 17:04:25 -0300

    Add better menu styles.

--- header.php
+++ header.php
@@ -45,7 +45,11 @@
 			</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'underskeleton' ); ?></button>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<span class="bars"></span>
+					<span class="bars"></span>
+					<span class="bars"></span>
+				</button>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 			</nav><!-- #site-navigation -->
 
