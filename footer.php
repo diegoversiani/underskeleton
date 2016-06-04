cd867b0b0071306c360ad572bdb2980cfd3060dd
Diego Versiani <diegoversiani@gmail.com>
2016-06-04 10:42:05 -0300

    Update style.css theme identification with new domains.

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
   		<div class="site-info">
   			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underskeleton' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'underskeleton' ), 'WordPress' ); ?></a>
   			<span class="sep"> | </span>
-  			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underskeleton' ), __( 'Underskeleton' , 'underskeleton'), __( '<a href="http://dvlopes.github.io/underskeleton" rel="designer">Diego Versiani</a>' , 'underskeleton') ); ?>
+  			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underskeleton' ), __( 'Underskeleton' , 'underskeleton'), __( '<a href="http://getunderskeleton.com" rel="designer">getunderskeleton.com</a>' , 'underskeleton') ); ?>
   		</div><!-- .site-info -->
 
     </div><!-- .row -->
