836860f0430c56467e4cc249d7bc414a50fd1743
Diego Versiani <diegoversiani@gmail.com>
2016-06-07 18:20:17 -0300

    Remove unnecessary gulp plugins instances from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,7 @@ var basePaths = {
 
 // Defining requirements
 var gulp = require('gulp');
-// var plumber = require('gulp-plumber');
-// var sass = require('gulp-sass');
-// var watch = require('gulp-watch');
-// var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
-// var concat = require('gulp-concat');
-// var uglify = require('gulp-uglify');
-// var merge2 = require('merge2');
-// var ignore = require('gulp-ignore');
-// var rimraf = require('gulp-rimraf');
 
 // Run: 
 // gulp copy-assets. 
