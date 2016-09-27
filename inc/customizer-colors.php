d5e9d4cbdd2a5bc2b78f0efd12e7b88ac581f1aa
diegoversiani <diegoversiani@gmail.com>
2016-09-27 18:22:21 -0300

    Add heading color setting to customizer.

--- inc/customizer-colors.php
+++ inc/customizer-colors.php
@@ -30,6 +30,24 @@ function underskeleton_colors_customizer( $wp_customize ) {
             'priority' => 10,
         )
         ));
+
+    /* SETTING: HEADING COLOR */
+    $wp_customize->add_setting( 'underskeleton_heading_color', array(
+        'default'   => '#d72d5c',
+        'type'      => 'theme_mod',
+        'capability' => 'edit_theme_options',
+        'transport' => 'refresh'
+        ));
+    $wp_customize->add_control( new WP_Customize_Color_Control( 
+        $wp_customize,
+        'underskeleton_heading_color',
+        array(
+            'label' => __( 'Heading Color', 'underskeleton' ),
+            'section' => 'colors',
+            'settings' => 'underskeleton_heading_color',
+            'priority' => 10,
+        )
+        ));
 }
 add_action('customize_register', 'underskeleton_colors_customizer');
 
@@ -37,12 +55,15 @@ add_action('customize_register', 'underskeleton_colors_customizer');
 
 function underskeleton_colors_customizer_output() {
     ?>
-    <!--Customizer CSS--> 
+    <!--Customizer Colors-->
     <style type="text/css">
-        body { color: <?php echo esc_attr( get_theme_mod( 'underskeleton_text_color', '#222' ) ); ?>; } /* default text color */
+        /* default text color */
+        body { color: <?php echo esc_attr( get_theme_mod( 'underskeleton_text_color', '#222' ) ); ?>; }
+        /* Headings */
+        h1, h2, h3, h4, h5, h6 { color: <?php echo esc_attr( get_theme_mod( 'underskeleton_heading_color', '#d72d5c' ) ); ?>; }
 
     </style> 
-    <!--/Customizer CSS-->
+    <!--/Customizer Colors-->
     <?php
 }
 add_action('wp_head', 'underskeleton_colors_customizer_output');
