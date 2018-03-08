0d4c2069fa7537c98345c35e810c1cae24f9af59
Diego Versiani <diegoversiani@gmail.com>
2018-03-08 17:51:47 +0100

    Fix woocommerce product search form template.

--- woocommerce/product-searchform.php
+++ woocommerce/product-searchform.php
@@ -25,6 +25,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="screen-reader-text" for="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>"><?php _e( 'Search for:', 'underskeleton' ); ?></label>
-	<input type="search" id="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?> class="search-field" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'underskeleton' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'underskeleton' ); ?>" /><input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'underskeleton' ); ?>" />
+	<input type="search" id="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'underskeleton' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'underskeleton' ); ?>" /><input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'underskeleton' ); ?>" />
 	<input type="hidden" name="post_type" value="product" />
 </form>
